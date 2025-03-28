03b516d07396a26598d2dfea1d9819431e15fea6
Dang Qui <dangdinhqui2001@gmail.com>
2025-03-29 02:05:30 +0700

    Video 4 - HoiDanIT

--- App.tsx
+++ App.tsx
@@ -4,12 +4,17 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.hearder}>Test rende web - Hello word!</Text>
+      <Text style={[styles.hearder, styles.marginBottom]} >Hello Word 1</Text> 
+      <Text style={[styles.footer, styles.marginBottom]}>Hello Word 2</Text> 
+      <Text style={styles.parent}>
+        Hello Word 3 
+        <Text style={styles.children}>- Hỏi Dân IT</Text></Text> 
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// StyleSheet không phải là CSS
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -20,8 +25,41 @@ const styles = StyleSheet.create({
 
   hearder: {
     fontSize: 30,
-    fontWeight: "bold",
-    color: "green"
-  }
+    fontWeight: "bold", // Ngang "600"
+    color: "green",
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 10,
+    padding: 10
+  },
 
+  footer: {
+    fontSize: 30,
+    fontWeight: "bold", // Ngang "600"
+    color: "blue",
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 10,
+    padding: 10
+  },
+
+  // Không có fontWeight
+  parent: {
+    color: "red",
+    fontSize: 30,
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 10,
+    padding: 10
+  },
+
+  children: {
+    color: "pink",
+    fontSize: 25,
+    fontWeight: "600", // Ngang "bold"
+  },
+
+  marginBottom: {
+    marginBottom: 10
+  }
 });
